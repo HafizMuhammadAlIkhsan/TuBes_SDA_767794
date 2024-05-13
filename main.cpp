9febba2587eee5298b9fc578865f250323f56eb9
HafizMuhammadAlIkhsan <hafiz.muhammad.tif23@polban.ac.id>
2024-05-13 21:06:48 +0700

    Membuat kerangka tampilan menu

--- main.cpp
+++ main.cpp
@@ -2,5 +2,34 @@
 
 int main()
 {
-    
+    gudang root;
+    addrBar hasil;
+    int opsi, level = 2, i = 1;
+    puts("masuk");
+    createTree(&root);
+    insertBarang(&root, NULL, "King Jonats", 0);
+    puts("masuk tampil");
+    hasil = searchGudang(root.root, "King Jonats");
+    puts("selesai cari root");
+    insertBarang(&root, hasil, "Makanan", 0);
+    insertBarang(&root, hasil, "Minuman", 0);
+    insertBarang(&root, hasil, "Kosmetik", 0);
+    puts("cari makanan");
+    hasil = searchGudang(root.root, "Makanan");
+    puts("selesai cari makanan");
+    insertBarang(&root, hasil, "Makanan ringan", 0);
+    insertBarang(&root, hasil, "Makanan berat", 0);
+    puts("cari minuman");
+    hasil = searchGudang(root.root, "Minuman");
+    puts("selesai cari minuman");
+    insertBarang(&root, hasil, "Minuman bersoda", 0);
+    insertBarang(&root, hasil, "Minuman berakohol", 0);
+    puts("cari kosmetik");
+    hasil = searchGudang(root.root, "Kosmetik");
+    puts("selesai cari minuman");
+    insertBarang(&root, hasil, "Pelembap", 0);
+    insertBarang(&root, hasil, "Pemutih (deniggafy)", 0);
+    system("cls");
+    puts("Sub Kategori");
+    tampilSubKategori(root.root, level, i);
 }
