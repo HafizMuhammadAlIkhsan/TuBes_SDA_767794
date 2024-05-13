cbfa173c3a294229cf39852794634e9f07dd64d0
HafizMuhammadAlIkhsan <hafiz.muhammad.tif23@polban.ac.id>
2024-05-13 20:58:54 +0700

    mengubah record dan menambahkan modul alokasi dan insert barang

--- header.h
+++ header.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #define nama(P) (P)->nama 
 #define pr(P) (P)->pr
@@ -35,6 +36,15 @@ typedef struct kasir
     addrKas akhir = NULL;  
 } kasir;
 
+/* barang yang dipesan oleh pembeli */
+typedef struct dibeli *addrBel;
+typedef struct dibeli
+{
+    addrBar namBar;
+    int jumlah;
+    addrBel nextBar;
+} dibeli;
+
 /* pembeli yang memesan barang */
 typedef struct pembeli *addrPem;
 typedef struct pembeli           
@@ -44,26 +54,21 @@ typedef struct pembeli
     addrPem next;
 } pembeli;
 
-/* barang yang dipesam oleh pembeli */
-typedef struct dibeli *addrBel;
-typedef struct dibeli
-{
-    addrBar namBar;
-    int jumlah;
-    addrBel nextBar;
-};
-
 /* Membuat nama dari Toko */
 void createTree(gudang *x);
 
+addrBar alokBarang(infotype nama, int harga);
+
+addrBar insertBarang(gudang *root, addrBar pr, infotype nama, int harga);
+
 /* Menampilkan list dari subkatergori barang yang ada di gudang toko */
-void tampilSubKategori (addrBar nama);
+void tampilSubKategori(addrBar nama, int level, int i);
 
 /* Modul yang digunakan untuk membuat LL berupa barang yang dibeli oleh pembeli */
-void beliBarang (addrPem pembeli, addrBel barang);
+void beliBarang(addrPem pembeli, addrBel barang);
 
 /* Mencari semua barang yang ada di gudang toko, sudah termasuk kategori dan subkategori */
-addrBar searchGudang (addrBar root, infotype cariGudang);
+addrBar searchGudang(addrBar root, char cariGudang[]);
 // variabel cariGudang ini maksudnya untuk mencari kategori, subkategori, dan barang
 
 #endif
