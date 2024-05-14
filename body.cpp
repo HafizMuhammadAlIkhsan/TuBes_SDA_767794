40ef9c44c6735a362c46d371fd5e4c2faf4a6111
Sulthan Aulia Rahman <sulthan.aulia.tif23@polban.ac.id>
2024-05-14 09:17:00 +0700

    :clown_face: kesalahan ngepush

--- body.cpp
+++ body.cpp
@@ -68,6 +68,16 @@ addrBar insertBarang(gudang *root, addrBar pr, infotype nama, int harga)
     return p;
 }
 
+void tambahstock()
+{
+
+}
+
+void kurangistock()
+{
+
+}
+
 void tampilkanGudang(addrBar node, int i)
 {
     int j = 0;
@@ -188,47 +198,4 @@ addrBar searchGudang(addrBar root, char cariGudang[])
         }
     } while (pr(p) != NULL);
     return NULL;
-}
-
-int main()
-{
-    gudang root;
-    addrBar hasil;
-    int opsi, level = 1, i = 1;
-    puts("masuk");
-    createTree(&root);
-    insertBarang(&root, NULL, "King Jonats", 0);
-    puts("masuk tampil");
-    hasil = searchGudang(root.root, "King Jonats");
-    puts("selesai cari root");
-    insertBarang(&root, hasil, "Makanan", 0);
-    insertBarang(&root, hasil, "Minuman", 0);
-    insertBarang(&root, hasil, "Kosmetik", 0);
-    puts("cari makanan");
-    hasil = searchGudang(root.root, "Makanan");
-    puts("selesai cari makanan");
-    insertBarang(&root, hasil, "Makanan ringan", 0);
-    insertBarang(&root, hasil, "Makanan berat", 0);
-    puts("cari minuman");
-    hasil = searchGudang(root.root, "Minuman");
-    puts("selesai cari minuman");
-    insertBarang(&root, hasil, "Minuman bersoda", 0);
-    insertBarang(&root, hasil, "Minuman berakohol", 0);
-    puts("cari kosmetik");
-    hasil = searchGudang(root.root, "Kosmetik");
-    puts("selesai cari minuman");
-    insertBarang(&root, hasil, "Pelembap", 0);
-    insertBarang(&root, hasil, "Pemutih (deniggafy)", 0);
-    system("cls");
-    puts("Kategori");
-    tampilkanKategori(root.root,level,i);
-    tampilSubKategori(root.root, level, i);
-    tampilkanGudang(root.root, 0);
-
-    puts("Menu kasir");
-    puts("1. Lihat Kategori");
-    puts("2. Lihat Subkategori");
-    puts("3. Lihat Barang");
-    puts("4. Beli Barang");
-    puts("5. Lihat Barang");
 }
