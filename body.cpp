1f640fd7b7f45ec3f07390a6fcc62f820a073f06
Sulthan Aulia Rahman <sulthan.aulia.tif23@polban.ac.id>
2024-05-14 09:12:05 +0700

    :sparkles: tampilkanKategori modul

--- body.cpp
+++ body.cpp
@@ -99,6 +99,32 @@ void tampilkanGudang(addrBar node, int i)
     }
 }
 
+void tampilkanKategori(addrBar root,int level,int i) 
+{
+    addrBar current;
+    current = root->fs;
+    if (root == NULL)
+    {
+        return;
+    }
+    while (current != NULL)
+    {
+        if (level == 1)
+        {
+            printf("%d. %s\n",i,current->nama);
+        }
+        else
+        {
+            tampilkanKategori(current, level - 1, i);
+            i++;
+        }
+        current = current->nb;
+        i++;
+    }   
+    
+    
+}
+
 void tampilSubKategori(addrBar root, int level, int i)
 {
     addrBar current = fs(root);
@@ -161,4 +187,47 @@ addrBar searchGudang(addrBar root, char cariGudang[])
         }
     } while (pr(p) != NULL);
     return NULL;
+}
+
+int main()
+{
+    gudang root;
+    addrBar hasil;
+    int opsi, level = 1, i = 1;
+    puts("masuk");
+    createTree(&root);
+    insertBarang(&root, NULL, "King Jonats", 0);
+    puts("masuk tampil");
+    hasil = searchGudang(root.root, "King Jonats");
+    puts("selesai cari root");
+    insertBarang(&root, hasil, "Makanan", 0);
+    insertBarang(&root, hasil, "Minuman", 0);
+    insertBarang(&root, hasil, "Kosmetik", 0);
+    puts("cari makanan");
+    hasil = searchGudang(root.root, "Makanan");
+    puts("selesai cari makanan");
+    insertBarang(&root, hasil, "Makanan ringan", 0);
+    insertBarang(&root, hasil, "Makanan berat", 0);
+    puts("cari minuman");
+    hasil = searchGudang(root.root, "Minuman");
+    puts("selesai cari minuman");
+    insertBarang(&root, hasil, "Minuman bersoda", 0);
+    insertBarang(&root, hasil, "Minuman berakohol", 0);
+    puts("cari kosmetik");
+    hasil = searchGudang(root.root, "Kosmetik");
+    puts("selesai cari minuman");
+    insertBarang(&root, hasil, "Pelembap", 0);
+    insertBarang(&root, hasil, "Pemutih (deniggafy)", 0);
+    system("cls");
+    puts("Kategori");
+    tampilkanKategori(root.root,level,i);
+    tampilSubKategori(root.root, level, i);
+    tampilkanGudang(root.root, 0);
+
+    puts("Menu kasir");
+    puts("1. Lihat Kategori");
+    puts("2. Lihat Subkategori");
+    puts("3. Lihat Barang");
+    puts("4. Beli Barang");
+    puts("5. Lihat Barang");
 }
