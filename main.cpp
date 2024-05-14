958579f90259d340b41189706f00cf1d0eb5381a
Fauzan Rizky Ramadhan <fauzanrizky821@gmail.com>
2024-05-14 11:35:49 +0700

    Halo

--- main.cpp
+++ main.cpp
@@ -53,98 +53,98 @@ int main()
     puts("");
     tampilkanGudang(root.root, 0);
 
-    // int nilai, posisi, opsi, returnValue;
-    // char* nama;
-    // do
-    // {
-    //     system("cls");
-    //     puts("(1) Menu Kasir");
-    //     puts("(2) Menu Gudang");
-    //     puts("(0) Keluar program");
+    int nilai, posisi, opsi, returnValue;
+    char* nama;
+    do
+    {
+        system("cls");
+        puts("(1) Menu Kasir");
+        puts("(2) Menu Gudang");
+        puts("(0) Keluar program");
 
-    //     printf("Pilih opsi: ");
-    //     scanf("%d", &opsi);
-    //     switch (opsi)
-    //     {
-    //     case 1:
-    //         /* Menu Kasir */
-    //         break;
-    //     case 2:
-    //         do
-    //         {
-    //             system("cls");
-    //             puts("(1) Tambah Kategori");
-    //             puts("(2) Tambah barang baru");
-    //             puts("(3) Tambah stok barang");
-    //             puts("(4) Tampilkan stok barang");
-    //             puts("(0) Kembali");
+        printf("Pilih opsi: ");
+        scanf("%d", &opsi);
+        switch (opsi)
+        {
+        case 1:
+            /* Menu Kasir */
+            break;
+        case 2:
+            do
+            {
+                system("cls");
+                puts("(1) Tambah Kategori");
+                puts("(2) Tambah barang baru");
+                puts("(3) Tambah stok barang");
+                puts("(4) Tampilkan stok barang");
+                puts("(0) Kembali");
 
-    //             printf("Pilih opsi: ");
-    //             scanf("%d", &opsi);
+                printf("Pilih opsi: ");
+                scanf("%d", &opsi);
 
-    //             switch (opsi)
-    //             {
-    //             case 1:
-    //                 do
-    //                 {
-    //                     system("cls");
-    //                     puts("(1) Tambah Kategori");
-    //                     puts("(2) Tambah Sub Kategori");
-    //                     puts("(0) Kembali");
+                switch (opsi)
+                {
+                case 1:
+                    do
+                    {
+                        system("cls");
+                        puts("(1) Tambah Kategori");
+                        puts("(2) Tambah Sub Kategori");
+                        puts("(0) Kembali");
+                        
+                        printf("Pilih opsi: ");
+                        scanf("%d", &opsi);
+                        switch (opsi)
+                        {
+                        case 1:
+                            printf("Nama kategori baru: ");
+                            scanf(" %s", &nama);
+                            break;
+                        case 2:
+                            /* code */
+                            break;
+                        case 0:
+                            puts("Kembali ke menu sebelumnya...");
+                            break;
+                        default:
+                            printf("Inputkan salah satu opsi di atas!\n");
+                            puts("===============================================================");
+                            break;
+                        }
+                    } while (opsi != 0);
+                    opsi = 99;
+                    break;
 
-    //                     printf("Pilih opsi: ");
-    //                     scanf("%d", &opsi);
-    //                     switch (opsi)
-    //                     {
-    //                     case 1:
-    //                         printf("Nama kategori baru: ");
-    //                         scanf(" %s", &nama);
-    //                         break;
-    //                     case 2:
-    //                         /* code */
-    //                         break;
-    //                     case 0:
-    //                         puts("Kembali ke menu sebelumnya...");
-    //                         break;
-    //                     default:
-    //                         printf("Inputkan salah satu opsi di atas!\n");
-    //                         puts("===============================================================");
-    //                         break;
-    //                     }
-    //                 } while (opsi != 0);
-    //                 opsi = 99;
-    //                 break;
+                case 2:
 
-    //             case 2:
+                    break;
 
-    //                 break;
+                case 3:
 
-    //             case 3:
-
-    //                 break;
-    //             case 4:
-    //                 puts("Sub Kategori");
-    //                 tampilSubKategori(root.root, level, i);
-    //                 tampilkanGudang(root.root, 0);
-    //                 break;
-    //             case 0:
-    //                 puts("Kembali ke menu sebelumnya...");
-    //                 break;
-    //             default:
-    //                 printf("Inputkan salah satu opsi di atas!\n");
-    //                 puts("===============================================================");
-    //                 break;
-    //             }
-    //         } while (opsi != 0);
-    //         opsi = 99;
-    //         break;
-    //     case 0:
-    //         puts("Keluar ke program...");
-    //         break;
-    //     default:
-    //         printf("Inputkan salah satu opsi di atas!\n");
-    //         puts("===============================================================");
-    //         break;
-    //     }
-    // } while (opsi != 0);
+                    break;
+                case 4:
+                    puts("Sub Kategori");
+                    tampilSubKategori(root.root, level, i);
+                    tampilkanGudang(root.root, 0);
+                    break;
+                case 0:
+                    puts("Kembali ke menu sebelumnya...");
+                    break;
+                default:
+                    printf("Inputkan salah satu opsi di atas!\n");
+                    puts("===============================================================");
+                    break;
+                }
+            } while (opsi != 0);
+            opsi = 99;
+            break;
+        case 0:
+            puts("Keluar ke program...");
+            break;
+        default:
+            printf("Inputkan salah satu opsi di atas!\n");
+            puts("===============================================================");
+            break;
+        }
+    } while (opsi != 0);
 }
